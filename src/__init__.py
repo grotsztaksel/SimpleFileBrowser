2f10d6f5c71671af84ccefe410e166ca98edff20
grotsztaksel <grotsztaksel@o2.pl>
2021-11-19 22:57:49 +0100

    Implement PercentBar

--- src/__init__.py
+++ src/__init__.py
@@ -5,9 +5,10 @@ Created on 19.11.2021 16:13 11
 @author: Piotr Gradkowski <grotsztaksel@o2.pl>
 """
 
-__all__ = ['MainWindow']
+__all__ = ['MainWindow', 'PercentBar']
 __date__ = '2021-11-19'
 __authors__ = ["Piotr Gradkowski <grotsztaksel@o2.pl>"]
 
 from .mainwindow import MainWindow
 from .utils import isText
+from .percent_bar import PercentBar
---
+++ src/percent_bar.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 19.11.2021 21:52 11
+ 
+@author: Piotr Gradkowski <grotsztaksel@o2.pl>
+
+Custom widget used to display percentage of data analyzed and percentage of data fulfilling some criteria
+"""
+
+__all__ = ['PercentBar']
+__date__ = '2021-11-19'
+__authors__ = ["Piotr Gradkowski <grotsztaksel@o2.pl>"]
+
+from PyQt5.QtCore import Qt, QRect, pyqtSlot, QSize
+
+from PyQt5.QtGui import QPaintEvent, QColor, QPen, QPainter
+from PyQt5.QtWidgets import QWidget
+
+
+class PercentBar(QWidget):
+    def __init__(self, parent, tooltipFormat=None):
+        super(PercentBar, self).__init__(parent)
+        self.setMinimumSize(QSize(32, 16))
+        self.assessed = 0.0
+        self.true = 0.0
+        self.tf = "Analyzed: {}%\nTrue: {}%"
+
+        self.unassessedColor = QColor(Qt.gray)
+        self.assessedColor = QColor(Qt.white)
+        self.trueColor = QColor(Qt.darkBlue)
+
+        if tooltipFormat is not None:
+            self.setTooltipFormat(tooltipFormat)
+
+    def setTooltipFormat(self, tf: str):
+        """
+        Sets the tooltip format
+        :param tf:
+        :return:
+        """
+        if not isinstance(tf, str):
+            return
+        self.tf = tf
+
+    @pyqtSlot(float)
+    def setPercentAssessed(self, p) -> None:
+        """
+        Set the number between 0.0 and 1.0 that tells what percent of available data has been assessed
+        :param p:
+        """
+        if 0.0 <= p <= 1.0:
+            self.assessed = p
+            self.repaint()
+
+    @pyqtSlot(float)
+    def setPercentTrue(self, p) -> None:
+        """
+        Set the number between 0.0 and 1.0 that tells what percent of assessed data fulfills given criteria
+        :param p:
+        """
+        if 0.0 <= p <= 1.0:
+            self.true = p
+            self.repaint()
+
+    def paintEvent(self, e: QPaintEvent) -> None:
+        rec = self.geometry()
+        print(rec.size())
+        p = QPainter(self)
+        p.setPen(Qt.NoPen)
+        b = p.brush()
+        b.setColor(self.unassessedColor)
+        b.setStyle(Qt.SolidPattern)
+        p.setBrush(b)
+        p.drawRect(rec)
+        b.setColor(self.assessedColor)
+        p.setBrush(b)
+        if self.assessed == 0:
+            return
+        w = rec.width() * self.assessed
+        rec = QRect(rec.topLeft(),  QSize(int(w), rec.height()))
+        p.drawRect(rec)
+        b.setColor(self.trueColor)
+        p.setBrush(b)
+        if self.true == 0.0:
+            return
+        w = rec.width() * self.true
+        rec = QRect(rec.topLeft(),  QSize(int(w), rec.height()))
+        p.drawRect(rec)
+        print(14)
