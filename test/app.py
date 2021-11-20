35aa440248837185fc9b32506f911335b70a7b89
grotsztaksel <grotsztaksel@o2.pl>
2021-11-20 19:27:57 +0100

    Add app in tests

---
+++ test/app.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 20.11.2021 19:05 11
+ 
+@author: Piotr Gradkowski <grotsztaksel@o2.pl>
+"""
+
+__date__ = '2021-11-20'
+__authors__ = ["Piotr Gradkowski <grotsztaksel@o2.pl>"]
+
+import sys
+
+from PyQt5.QtWidgets import QApplication
+
+# initialize an application to prevent some QObjects from bombing out
+app = QApplication(sys.argv)
